57031ed085fa539246e71c2b1d88dd339854a0c7
Viktorhs <victor.hugo.gba@hotmail.com.br>
2023-04-28 16:26:35 -0300

    handling errors

--- Program.cs
+++ Program.cs
@@ -9,11 +9,21 @@ namespace Xadrez_console // Note: actual namespace depends on the project name.
     static void Main(string[] args)
     {
 
-      Tabuleiro tab = new Tabuleiro(8, 8);
-      tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-      tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-      tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-      Tela.ImprimirTabuleiro(tab);
+      try
+      {
+        Tabuleiro tab = new Tabuleiro(8, 8);
+        tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+        tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+        tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+        Tela.ImprimirTabuleiro(tab);
+      }
+      catch (TabuleiroException e)
+      {
+        System.Console.WriteLine(e.Message);
+      }
+
+
+
 
       Console.ReadLine();
     }
